afb1dce15dbc21f367b0179839ff083fc3f4b298
ayanokoujifl <guleite3@gmail.com>
2025-12-04 11:01:01 -0300

    impl wallet useCase

--- src/main/java/com/luisleite/carteira/domain/usecase/WalletUseCaseImpl.java
+++ src/main/java/com/luisleite/carteira/domain/usecase/WalletUseCaseImpl.java
@@ -1,4 +1,23 @@
 package com.luisleite.carteira.domain.usecase;
 
-public class WalletUseCaseImpl {
+import com.luisleite.carteira.domain.model.Wallet;
+import com.luisleite.carteira.domain.ports.input.WalletCreateUseCase;
+import com.luisleite.carteira.domain.ports.output.WalletRepositoryPort;
+
+public class WalletUseCaseImpl implements WalletCreateUseCase {
+
+    private final WalletRepositoryPort walletRepositoryPort;
+
+    public WalletUseCaseImpl(WalletRepositoryPort walletRepositoryPort) {
+        this.walletRepositoryPort = walletRepositoryPort;
+    }
+
+    @Override
+    public Wallet create(String userId) {
+        if (walletRepositoryPort.existByUserId(userId)) {
+            throw new RuntimeException("This user already has a wallet");
+        }
+        var newWallet = new Wallet(userId);
+        return walletRepositoryPort.save(newWallet);
+    }
 }
