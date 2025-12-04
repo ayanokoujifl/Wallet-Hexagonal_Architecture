23b40472c1cf25088706e3a80518d70084220147
ayanokoujifl <guleite3@gmail.com>
2025-12-04 11:28:36 -0300

    wallet repo port impl

---
+++ src/main/java/com/luisleite/carteira/application/config/WalletConfig.java
@@ -0,0 +1,4 @@
+package com.luisleite.carteira.application.config;
+
+public class WalletConfig {
+}
--- src/main/java/com/luisleite/carteira/infrastructure/persistence/WalletRepositoryGateway.java
+++ src/main/java/com/luisleite/carteira/infrastructure/persistence/WalletRepositoryGateway.java
@@ -2,17 +2,31 @@ package com.luisleite.carteira.infrastructure.persistence;
 
 import com.luisleite.carteira.domain.model.Wallet;
 import com.luisleite.carteira.domain.ports.output.WalletRepositoryPort;
+import com.luisleite.carteira.infrastructure.persistence.entity.WalletEntity;
+import com.luisleite.carteira.infrastructure.persistence.mapper.WalletPersistenceMapper;
+import com.luisleite.carteira.infrastructure.persistence.repository.JpaWalletRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WalletRepositoryGateway implements WalletRepositoryPort {
+
+    private final JpaWalletRepository repository;
+    private final WalletPersistenceMapper mapper;
+
+    public WalletRepositoryGateway(JpaWalletRepository repository, WalletPersistenceMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
     @Override
     public Wallet save(Wallet wallet) {
-        return null;
+        WalletEntity entity = mapper.toEntity(wallet);
+        entity = repository.save(entity);
+        return mapper.toDomain(entity);
     }
 
     @Override
     public boolean existByUserId(String userId) {
-        return false;
+        return repository.existsByUserId(userId);
     }
 }
