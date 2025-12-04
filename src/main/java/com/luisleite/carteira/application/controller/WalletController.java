bc64e8d189aa064173c6dbc03d13c8dc4ee4b85b
ayanokoujifl <guleite3@gmail.com>
2025-12-04 11:42:11 -0300

    controller impl create

--- src/main/java/com/luisleite/carteira/application/controller/WalletController.java
+++ src/main/java/com/luisleite/carteira/application/controller/WalletController.java
@@ -1,4 +1,34 @@
 package com.luisleite.carteira.application.controller;
 
+import com.luisleite.carteira.application.controller.dto.WalletCreateRequest;
+import com.luisleite.carteira.application.controller.dto.WalletResponse;
+import com.luisleite.carteira.domain.model.Wallet;
+import com.luisleite.carteira.domain.ports.input.WalletCreateUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wallets")
 public class WalletController {
+
+    private final WalletCreateUseCase walletCreateUseCase;
+
+    public WalletController(WalletCreateUseCase walletCreateUseCase) {
+        this.walletCreateUseCase = walletCreateUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<WalletResponse> create(@RequestBody WalletCreateRequest request) {
+        Wallet wallet = walletCreateUseCase.create(request.userId());
+        var response = new WalletResponse(
+                wallet.getId(),
+                wallet.getUserId(),
+                wallet.getBalance(),
+                wallet.isActive()
+        );
+        return ResponseEntity.ok(response);
+    }
 }
